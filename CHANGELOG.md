989b7395d12a67b5f6362c245d7353e1194e14aa
Martin Loetzsch <martin.loetzsch@gmail.com>
2017-09-21 17:58:47 +0200

    release 1.1.0
    - Made the config and click commands discoverable in [mara-app](https://github.com/mara/mara-app) >= 1.2.0
    - Documentation updates

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+# Changelog
+
+## 1.1.0 
+*2017-09-21 
+
+- Made the config and click commands discoverable in [mara-app](https://github.com/mara/mara-app) >= 1.2.0
+- Documentation updates
+
+
+
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bingads-performance-downloader',
-    version='1.0.0',
+    version='1.1.0',
 
     description="Downloads data from the BingAds Api to local files for usage in a data warehouse",
 
