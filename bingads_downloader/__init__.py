2115511952325392059acd7d794dc105b2b4755e
Martin Loetzsch <martin.loetzsch@gmail.com>
2019-04-12 23:47:32 +0200

    Change MARA_XXX variables to functions to delay importing of imports (requires updating mara-app to 2.0.0)

--- bingads_downloader/__init__.py
+++ bingads_downloader/__init__.py
@@ -1,5 +1,27 @@
-from bingads_downloader import cli, config
+"""Make the functionalities of this package auto-discoverable by mara-app"""
 
-MARA_CONFIG_MODULES = [config]
 
-MARA_CLICK_COMMANDS = [cli.download_data, cli.refresh_oauth2_token]
+def MARA_CONFIG_MODULES():
+    from . import config
+    return [config]
+
+
+def MARA_FLASK_BLUEPRINTS():
+    return []
+
+
+def MARA_AUTOMIGRATE_SQLALCHEMY_MODELS():
+    return []
+
+
+def MARA_ACL_RESOURCES():
+    return {}
+
+
+def MARA_CLICK_COMMANDS():
+    from . import cli
+    return [cli.download_data, cli.refresh_oauth2_token]
+
+
+def MARA_NAVIGATION_ENTRIES():
+    return []
